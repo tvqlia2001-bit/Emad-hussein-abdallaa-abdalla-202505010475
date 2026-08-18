d34ad5aff1797cf067fc450e5d580c039b449d7b
tvqlia2001-bit <tvqlia2001@gmail.com>
2026-08-18 07:25:55 +0000

    tutorial 3

---
+++ week_3/Main.java
@@ -0,0 +1,15 @@
+package week_3;
+
+public class Main {
+
+        Person p1 = new Person("Aisha", "P001");
+        Person p2 = new Lecturer("Sara", "L001");
+        Person p3 = new Student("Ahmed", "S001");
+
+        
+        p1.introduce();
+        p2.introduce();
+        p3.introduce();
+    }
+}
+
---
+++ week_3/Student.java
@@ -0,0 +1,11 @@
+   package week_3;
+   
+   class Student extends Person{
+   public Student(String name, String id){
+        super(name, id);
+    }
+    @Override
+    public void introduce(){
+        System.out.println("I am a student.");
+    }
+}
